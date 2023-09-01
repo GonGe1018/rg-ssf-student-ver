4f90fa83bc43a0755dc615f44dc52e32a39995f7
GonGe1018 <chaeho1234@naver.com>
2023-09-02 02:48:52 +0900

    commit

--- Assets/Script/CatController.cs
+++ Assets/Script/CatController.cs
@@ -1,52 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
-using Unity.VisualScripting.FullSerializer;
 using UnityEngine;
 
 public class CatController : MonoBehaviour
 {
-    public int HP = 30;
-    public bool isDied=false;
-
-    public int firstDir = 1;
-    public float changeDirTime;
-
-    int moveDir = 1;
-
-    float shotTimeMin = 0.3f;
-    float shotTimeMax = 1f;
-
-    //cat 총알 prefab 변수 선언
-
-    private void Start()
+    // Start is called before the first frame update
+    void Start()
     {
-        StartCoroutine(MoveDirection());
-        StartCoroutine(shootBullet());
+        
     }
 
-    private void Update()
+    // Update is called once per frame
+    void Update()
     {
-        transform.Translate(Vector3.right*moveDir*firstDir*Time.deltaTime);
-        if(HP<=0) { 
-            isDied = true;
-            gameObject.SetActive(false);
-        }
-
-    }
-
-    IEnumerator MoveDirection()
-    {
-        while (true)
-        {
-            yield return new WaitForSeconds(changeDirTime);
-            moveDir *= -1;
-        }
-    }
-
-    IEnumerator shootBullet()
-    {
-        //코드 작성 부분(아래 코드 지워주세요!)
-        yield return null;
         
     }
 }
--- Assets/Script/GameManger.cs
+++ Assets/Script/GameManger.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CatBulletController : MonoBehaviour
+public class GameManger : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
